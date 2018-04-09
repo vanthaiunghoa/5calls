6c42de35ffd32348761fa433ff60d8b6e1e9b5e6
Nick O'Neill <nick.oneill@gmail.com>
2018-04-09 16:46:37 -0700

    Pull user stats for the impact page.

--- src/components/myimpact/MyImpactPageContainer.tsx
+++ src/components/myimpact/MyImpactPageContainer.tsx
@@ -3,15 +3,18 @@ import { ApplicationState } from '../../redux/root';
 import { UserStatsState } from '../../redux/userStats';
 
 import { MyImpactPage } from './index';
+import { UserState } from '../../redux/userState';
 
 interface StateProps {
   readonly userStats: UserStatsState;
+  readonly userState?: UserState;
   readonly totalCount: number;
 }
 
 const mapStateToProps = (state: ApplicationState): StateProps => {
   return {
     userStats: state.userStatsState,
+    userState: state.userState,
     totalCount: state.remoteDataState.callTotal,
   };
 };
