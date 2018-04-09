fc57b99fbd83fc7a73bc0fdbf81beaf83c9c35b5
Nick O'Neill <nick.oneill@gmail.com>
2018-04-09 12:09:30 -0700

    Close login panel when clicking away.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import onClickOutside from 'react-onclickoutside';
 
 import { DonationContainer } from '../donation/';
 import { UserState, UserProfile } from '../../redux/userState/reducer';
@@ -10,53 +11,80 @@ interface Props {
   readonly currentUser?: UserState;
 }
 
-export const Header: React.StatelessComponent<Props> = (props: Props) => {
-  const authutil = new AuthUtil();
+interface State {
+  readonly userMenuHidden: boolean;
+  readonly currentUser?: UserState;
+}
 
-  let profile: UserProfile | undefined;
-  if (props.currentUser !== undefined) {
-    profile = props.currentUser.profile;
+const authutil = new AuthUtil();
+
+class HeaderImpl extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      userMenuHidden: true
+    };
   }
 
-  const loginClick = () => {
-    if (profile !== undefined) {
-      console.log('got a profile');
-      authutil.login();
+  handleClickOutside = () => {
+    if (!this.state.userMenuHidden) { this.toggleMenu(); }
+  }
+
+  toggleMenu() {
+    this.setState({ userMenuHidden: !this.state.userMenuHidden });
+  }
+
+  loginClick = () => {
+    if (this.props.currentUser && this.props.currentUser.profile) {
+      this.toggleMenu();
     } else {
       authutil.login();
     }
-  };
+  }
 
-  const logout = () => {
+  logout = () => {
     authutil.logout();
-  };
+    this.toggleMenu();
+  }
 
-  return (
-    <header className="logo__header" role="banner" >
-      <div className="logo__header__logo layout">
-        <Link to={`/`}>
-          <img src="/img/5calls-logo-small.png" alt="5 Calls" />
-        </Link>
-        {/* keep this around for teams / campaigns, but don't show for now */}
-        {/* <ul>
-          <li><Link className={props.postcards ? '' : 'active'} to="/">Calls</Link></li>
-          <li><Link className={props.postcards ? 'active' : ''} to="/postcards">Postcards</Link></li>
-        </ul> */}
-        <div className="userHeader">
-          <a onClick={loginClick}>
-            <img className="stars" src="/img/5calls-stars.png" alt="Make your voice heard" />
-          </a>
-          <p><a onClick={loginClick}>{profile ? profile.name : 'Login'}</a></p>
-          <div className="userHeader__menu">
-            <ul>
-              <li><Link to="/impact">My Impact</Link></li>
-              <li className="line"></li>
-              <li><a href="#" onClick={logout}><strong>Log out</strong></a></li>
-            </ul>
+  render() {
+    let profile: UserProfile | undefined;
+    if (this.props.currentUser !== undefined) {
+      profile = this.props.currentUser.profile;
+    }  
+
+    return (
+      <header className="logo__header" role="banner" >
+        <div className="logo__header__logo layout">
+          <Link to="/">
+            <img src="/img/5calls-logo-small.png" alt="5 Calls" />
+          </Link>
+          {/* keep this around for teams / campaigns, but don't show for now */}
+          {/* <ul>
+            <li><Link className={props.postcards ? '' : 'active'} to="/">Calls</Link></li>
+            <li><Link className={props.postcards ? 'active' : ''} to="/postcards">Postcards</Link></li>
+          </ul> */}
+          <div className="userHeader">
+            <a onClick={this.loginClick}>
+              <img className="stars" src="/img/5calls-stars.png" alt="Make your voice heard" />
+            </a>
+            <p><a onClick={this.loginClick}>{profile ? profile.name : 'Login'}</a></p>
+            { !this.state.userMenuHidden && 
+            <div className="userHeader__menu">
+              <ul>
+                <li><Link to="/impact">My Impact</Link></li>
+                <li className="line"/>
+                <li><a href="#" onClick={this.logout}><strong>Log out</strong></a></li>
+              </ul>
+            </div>
+            }
           </div>
         </div>
-      </div>
-      {<DonationContainer />}
-    </header>
-  );
-};
+        {<DonationContainer />}
+      </header>
+    );
+  }
+}
+
+export let Header = onClickOutside(HeaderImpl);
