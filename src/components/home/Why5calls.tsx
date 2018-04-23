87c521ddd44d9455c19dcf7f8f22dc1caa67d9df
Nick O'Neill <nick.oneill@gmail.com>
2018-04-22 19:22:19 -0500

    Tshirt display

--- src/components/home/Why5calls.tsx
+++ src/components/home/Why5calls.tsx
@@ -13,35 +13,24 @@ interface Props {
 export const Why5calls: React.StatelessComponent<Props> = (props: Props) => (
   <div className="hypothesis" >
     <header className="hypothesis__header">
-      <h1 className="hypothesis__title">{props.t('hypothesis.title')}</h1>
+      <h1 className="hypothesis__title">Make Your Shirt Heard</h1>
       <h2 className="hypothesis__subtitle">
         {/*tslint:disable-next-line:max-line-length*/}
-        5 Calls is the easiest and most effective way for citizens to make an impact in national and local politics
+        <a href={Constants.TSHIRTS_URL}>TWO WEEKS ONLY</a>: Grab a limited edition 5 Calls T-Shirt, the perfect attire for Town Halls, Protests and Marches!
       </h2>
-      <p>
-        {props.t('hypothesis.p1')}
-      </p>
-    </header>
-    <div className="hypothesis__text">
+      <div className="hypothesis__shirts">
+        <a href={Constants.TSHIRTS_URL}><img src="/img/5calls-shirts@2x.png" alt="5 Calls T-Shirts"/></a>
+      </div>
+      {/*tslint:disable-next-line:max-line-length*/}
+      <p>We teamed up with our friends at Cotton Bureau to bring you high-quality T-Shirts and Hats from our 5 Calls design team. Let people know you can Make Your Voice Heard. Proceeds go to supporting 5 Calls: <a href={Constants.TSHIRTS_URL}>Shop Now!</a></p>
       <CallCount
         totalCount={props.totalCount}
         t={i18n.t}
       />
-      <hr />
-      <a href={Constants.contact.apps}><img src="/img/5calls-apps.png" className="hypothesis__text__mobile" /></a>
-      <p dangerouslySetInnerHTML={{ __html: props.t('hypothesis.p3') }} />
-      <div className="subscribe">
-        {/*tslint:disable-next-line:max-line-length*/}
-        <form action="//5calls.us16.list-manage.com/subscribe/post?u=82a164d5fe7f51f4a4efb1f83&amp;id=624ef52208" method="post" target="popupwindow">
-          <label htmlFor="email"><strong>{props.t('footer.emailLabel')}</strong></label>
-          <span className="emailform">
-            <input type="text" placeholder="youremail@example.com" name="email" id="email" />
-            <input type="submit" value={props.t('footer.subscribe')} />
-          </span>
-        </form>
-      </div>
-      <div style={{'clear': 'both'}} />
-    </div>
+      <p>
+        {props.t('hypothesis.p1')}
+      </p>
+    </header>
   </div>
 );
 
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import { DonationContainer } from '../donation/';
+// import { DonationContainer } from '../donation/';
 
 interface Props {
   readonly postcards?: boolean;
@@ -21,7 +21,7 @@ export const Header: React.StatelessComponent<Props> = (props: Props) => {
         </ul> */}
         <img className="stars" src="/img/5calls-stars.png" alt="Make your voice heard" />
       </div>
-      {<DonationContainer />}
+      {/* {<DonationContainer />} */}
     </header>
   );
 };
